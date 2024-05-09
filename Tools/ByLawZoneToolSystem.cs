18dfe884f00c7c5fa96d9842d849537b4b874475
Nick Morrison <nickmorrison09@gmail.com>
2024-05-09 00:37:14 -0400

    Added ByLawZoneToolSystem

---
+++ Tools/ByLawZoneToolSystem.cs
@@ -0,0 +1,23 @@
+﻿using Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trejak.ZoningByLaw.Tools
+{
+    public partial class ByLawZoneToolSystem : GameSystemBase
+    {
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+        protected override void OnUpdate()
+        {
+            
+        }
+    }
+}
