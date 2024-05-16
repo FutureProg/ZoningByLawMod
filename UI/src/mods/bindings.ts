58393463f827fe5eeb08730acc6ddedd724c54da
Nick Morrison <nickmorrison09@gmail.com>
2024-05-16 00:44:43 -0400

    Set up bindings on the JS side

--- UI/src/mods/bindings.ts
+++ UI/src/mods/bindings.ts
@@ -1,11 +1,24 @@
 import { bindValue, trigger } from "cs2/api";
 import mod from '../../mod.json';
 import { ByLawZoneComponent, ByLawZoneData, ByLawZoneListItem } from "./types";
+import { Entity } from "cs2/bindings";
 
 
 export const byLawZoneList = bindValue<ByLawZoneListItem[]>(mod.fullname, "ByLawZoneList", []);
-export const selectedByLawData = bindValue<ByLawZoneComponent>(mod.fullname, "ByLaws");
+export const selectedByLawData = bindValue<ByLawZoneComponent>(mod.fullname, "SelectedByLawData");
+
+export const setActiveByLaw = (entity: Entity) => {
+    trigger(mod.fullname, "SetActiveByLaw", entity);
+}
 
 export const setByLawData = (byLawData: ByLawZoneComponent) => {
     trigger(mod.fullname, "SetByLawData", byLawData);
+}
+
+export const createNewByLaw = (byLawData : ByLawZoneComponent) => {
+    trigger(mod.fullname, "CreateNewByLaw", byLawData);
+}
+
+export const deleteByLaw = (entity: Entity) => {
+    trigger(mod.fullname, "DeleteByLaw", entity);
 }
