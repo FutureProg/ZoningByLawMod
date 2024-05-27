d78a0a7f49caba47794d37a3f6fff1a82b75934a
Nick Morrison <nickmorrison09@gmail.com>
2024-05-27 00:35:16 -0400

    got a prototype of previews working

--- Systems/ByLawRenderOverlaySystem.cs
+++ Systems/ByLawRenderOverlaySystem.cs
@@ -1,4 +1,5 @@
-﻿using Colossal.Mathematics;
+﻿using Colossal;
+using Colossal.Mathematics;
 using Game;
 using Game.Rendering;
 using Game.Tools;
@@ -22,8 +23,7 @@ namespace Trejak.ZoningByLaw.Systems
         private ByLawRenderToolSystem _bylawRenderToolSystem;
         private OverlayRenderSystem _overlayRenderSystem;
         private ToolSystem _toolSystem;
-
-        bool doOnce = true;
+        private GizmosSystem _gizmosSystem;
 
         protected override void OnCreate()
         {
@@ -31,7 +31,7 @@ namespace Trejak.ZoningByLaw.Systems
             _toolSystem = World.GetOrCreateSystemManaged<ToolSystem>();
             _overlayRenderSystem = World.GetOrCreateSystemManaged<OverlayRenderSystem>();
             _bylawRenderToolSystem = World.GetOrCreateSystemManaged<ByLawRenderToolSystem>();
-            doOnce = true;
+            _gizmosSystem = World.GetOrCreateSystemManaged<GizmosSystem>();
         }
 
         protected override void OnStopRunning()
@@ -43,7 +43,6 @@ namespace Trejak.ZoningByLaw.Systems
         {
             if (_toolSystem.activeTool != _bylawRenderToolSystem || !_bylawRenderToolSystem.byLawZoneData.HasValue)
             {
-                doOnce = true;
                 return;
             }
             var currentDrawPoint = _bylawRenderToolSystem.currentDrawPoint;
@@ -54,15 +53,11 @@ namespace Trejak.ZoningByLaw.Systems
                 {
                     overlayBuffer = _overlayRenderSystem.GetBuffer(out var overlayRenderHandle),
                     terrainPoint = currentDrawPoint.Value,
-                    bylawData = byLawZoneData.Value
+                    bylawData = byLawZoneData.Value,
+                    gizmoBatcher = _gizmosSystem.GetGizmosBatcher(out var gizmoJobHandle)
                 };
-                this.Dependency = renderPreviewJob.Schedule(JobHandle.CombineDependencies(this.Dependency, overlayRenderHandle));
+                this.Dependency = renderPreviewJob.Schedule(JobHandle.CombineDependencies(this.Dependency, overlayRenderHandle, gizmoJobHandle));
                 _overlayRenderSystem.AddBufferWriter(this.Dependency);
-                if (doOnce)
-                {
-                    Mod.log.Info("Rendering line with max height: " + math.min(100.0f, byLawZoneData.Value.height.max > 0 ? byLawZoneData.Value.height.max : 100.0f));
-                    doOnce = false;
-                }
             }
         }
 
@@ -75,6 +70,7 @@ namespace Trejak.ZoningByLaw.Systems
             public ByLawZoneData bylawData;
             public OverlayRenderSystem.Buffer overlayBuffer;
             public float3 terrainPoint;
+            public GizmoBatcher gizmoBatcher;
 
             public void Execute()
             {
@@ -86,44 +82,48 @@ namespace Trejak.ZoningByLaw.Systems
 
                 float depth = 6 * unitSizeMetres;
                 float lotSize = depth * width;
-                //while (true)
-                //{
-                //    bool passMinLotSize = bylawData.lotSize.min <= 0 || bylawData.lotSize.min <= lotSize;
-                //    bool passMaxLotSize = bylawData.lotSize.max <= 0 || bylawData.lotSize.max >= lotSize;
-                //    if (passMinLotSize && passMaxLotSize)
-                //    {
-                //        break;
-                //    }
+                while (true)
+                {
+                    bool passMinLotSize = bylawData.lotSize.min <= 0 || bylawData.lotSize.min <= lotSize;
+                    bool passMaxLotSize = bylawData.lotSize.max <= 0 || bylawData.lotSize.max >= lotSize;
+                    if (passMinLotSize && passMaxLotSize)
+                    {
+                        break;
+                    }
 
-                //    // Basically set a value that isn't possible
-                //    if ((passMinLotSize || passMaxLotSize) && bylawData.lotSize.max - bylawData.lotSize.min < unitSizeMetres)
-                //    {
-                //        break;
-                //    }
-                //    if (!passMinLotSize)
-                //    {
-                //        depth += unitSizeMetres;
-                //    }
-                //    if (!passMaxLotSize)
-                //    {
-                //        depth -= unitSizeMetres;
-                //    }
+                    // Basically set a value that isn't possible
+                    if ((passMinLotSize || passMaxLotSize) && bylawData.lotSize.max - bylawData.lotSize.min < unitSizeMetres)
+                    {
+                        break;
+                    }
+                    if (!passMinLotSize)
+                    {
+                        depth += unitSizeMetres;
+                    }
+                    if (!passMaxLotSize)
+                    {
+                        depth -= unitSizeMetres;
+                    }
 
-                //    lotSize = depth * width;
-                //}
+                    lotSize = depth * width;
+                }
                 // x and z horizontal plane
-                float x = width / 2f - terrainPoint.x;
-                float z = depth / 2f - terrainPoint.z;
-                float y = height + terrainPoint.y;
-
-                Bounds3 bounds = new Bounds3(new float3(x, terrainPoint.y, z), new float3(x + width, y, z + depth));
+                float x = terrainPoint.x - width / 2f;
+                float z = terrainPoint.z - depth / 2f;
+                float y = height + terrainPoint.y;                
 
                 // for now just draw a line of the correct height. A cube is a lot of work.
                 Color lineColor = Color.red;
-                float lineWidth = 4.5f;
-                overlayBuffer.DrawLine(lineColor, lineColor, lineWidth, 0, new Line3.Segment(terrainPoint, new float3(terrainPoint.x + 0.01f, y, terrainPoint.z + 0.01f)), lineWidth, new float2(1.5f, 1.5f));
-                overlayBuffer.DrawCircle(lineColor, Color.clear, lineWidth, 0, new float2(0, 0), terrainPoint + 0.1f, 50.0f);
+                float lineWidth = 1.5f;
+                overlayBuffer.DrawLine(lineColor, Color.clear, 
+                    lineWidth, 0, 
+                    new Line3.Segment(new float3(terrainPoint.x, terrainPoint.y, terrainPoint.z - depth/2f), new float3(terrainPoint.x, terrainPoint.y, terrainPoint.z + depth / 2f)), 
+                    width
+                ); // draws the lot size
 
+                float3 cubeCentre = terrainPoint+ new float3(0, height / 2f, 0);
+                float3 cubeScale = new float3(width, height, depth);
+                gizmoBatcher.DrawWireCube(cubeCentre, cubeScale, lineColor); // draws the building
             }
         }
 
