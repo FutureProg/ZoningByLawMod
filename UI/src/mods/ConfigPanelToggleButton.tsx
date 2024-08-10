2e3758c0828929b2452759c2211daee6f898d392
Nick Morrison <nickmorrison09@gmail.com>
2024-08-10 12:15:28 -0400

    Zone panel button only shows in zoning tool window now

--- UI/src/mods/ConfigPanelToggleButton.tsx
+++ UI/src/mods/ConfigPanelToggleButton.tsx
@@ -2,6 +2,7 @@ import { useValue } from "cs2/api"
 import { isConfigPanelOpen$, setConfigPanelOpen } from "./bindings"
 import { Button, FOCUS_AUTO, FOCUS_DISABLED } from "cs2/ui";
 import { ModuleRegistryExtend } from "cs2/modding";
+import { tool, toolbar } from "cs2/bindings";
 
 import styles from './configpaneltogglebutton.module.scss';
 import { VanillaComponentResolver } from "vanillacomponentresolver";
@@ -9,18 +10,22 @@ import { VanillaComponentResolver } from "vanillacomponentresolver";
 export const ConfigPanelToggleButton : ModuleRegistryExtend = (Component) => {
     return (props) => {
         const { children, ...otherProps } = props || {};
+        let activeTool = useValue(tool.activeTool$);
         let isPanelOpen = useValue(isConfigPanelOpen$);
         let onClick = () => {
             setConfigPanelOpen(!isPanelOpen);
         }
-        return (
-        <>            
+        let button = activeTool.id != tool.ZONE_TOOL? <></> : (
             <VanillaComponentResolver.instance.Section>
                 <Button className={styles.button + ' ' + VanillaComponentResolver.instance.toolButtonTheme.button} focusKey={FOCUS_DISABLED} variant="flat" onClick={onClick}>
                     <img src="coui://trejak_zbl/config-icon.svg" />
                     Zoning ByLaw Editor
                 </Button>            
             </VanillaComponentResolver.instance.Section>
+        );                
+        return (
+        <>            
+            {button}
             <Component {...otherProps}>                            
                 {children}
             </Component>                        
