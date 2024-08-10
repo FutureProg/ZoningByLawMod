549e4f23248f2fa18a10c855f993bb55f27f8088
Nick Morrison <nickmorrison09@gmail.com>
2024-08-10 17:43:03 -0400

    Added list of constraints to the editor

--- UI/src/mods/ByLawEditorView/ByLawEditorView.tsx
+++ UI/src/mods/ByLawEditorView/ByLawEditorView.tsx
@@ -1,9 +1,25 @@
+import { Scrollable } from 'cs2/ui';
 import styles from './ByLawEditorView.module.scss';
+import { useValue } from 'cs2/api';
+import { selectedByLawColor$, selectedByLawData$, selectedByLawName$ } from 'mods/bindings';
+import { ByLawItemType } from 'mods/types';
 
 export const ByLawEditorView = ({searchQuery} : {searchQuery?: string}) => {
+    let byLawData = useValue(selectedByLawData$);
+    let byLawName = useValue(selectedByLawName$);
+    let byLawColor = useValue(selectedByLawColor$);
+
+    let types = Object.keys(ByLawItemType)        
+        .filter((val) => isNaN(Number(val)) && val != 'None')        
+        .map((val) => val.split(/(?<![A-Z])(?=[A-Z])/).join(' '))
+        .filter((val) => searchQuery? val.toUpperCase().indexOf(searchQuery.toUpperCase()) > 0 : true);
+    let listItems = types.map((typeInfo, idx) => 
+        <div className={styles.listItem} key={idx}>{typeInfo}</div>
+    );
+
     return (
-        <div className={styles.view}>
-            Editor
-        </div>
+        <Scrollable className={styles.view}>
+            {listItems}
+        </Scrollable>
     )
 }
