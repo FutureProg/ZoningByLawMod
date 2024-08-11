ff4bc0dea6a72f4a62d66eb4b4c8741b23bae235
Nick Morrison <nickmorrison09@gmail.com>
2024-08-10 22:03:43 -0400

    Added more value text

--- UI/src/mods/components/ConstraintValueText/ConstraintValueText.tsx
+++ UI/src/mods/components/ConstraintValueText/ConstraintValueText.tsx
@@ -1,15 +1,31 @@
-import { BOUNDS_VALUE_DISABLED, ByLawConstraintType, ByLawItem, ByLawItemType } from "mods/types";
+import { BOUNDS_VALUE_DISABLED, ByLawConstraintType, ByLawItem, ByLawItemType, ByLawZoneType } from "mods/types";
 
 export default (props: {className?: string, item?: ByLawItem}) => {
     let text = "";
     switch(props.item?.constraintType) {
-        case ByLawConstraintType.Length: {
+        case ByLawConstraintType.Length:
+        case ByLawConstraintType.Count: {
             let value = props.item.valueBounds1;
-            let minText = value.min > BOUNDS_VALUE_DISABLED? `${value.min} metres` : "";
-            let maxText = value.max > BOUNDS_VALUE_DISABLED? `${value.max} metres` : "";            
+            let measurement = props.item?.constraintType == ByLawConstraintType.Length? 'm' : '';
+            let minText = value.min > BOUNDS_VALUE_DISABLED? `${value.min}${measurement}` : "";
+            let maxText = value.max > BOUNDS_VALUE_DISABLED? `${value.max}${measurement}` : "";            
             text = `${minText}${minText && maxText? " to " : ""}${maxText}`;
             break; 
-        }            
+        }
+        case ByLawConstraintType.MultiSelect: {            
+            switch(props.item!.byLawItemType) {
+                case ByLawItemType.LandUse: {
+                    let value = ByLawZoneType[props.item.valueByteFlag];                    
+                    let count = Object.keys(ByLawZoneType)
+                        .filter(key => !isNaN(Number(key)))                        
+                        .map((key, value) => ((Number(key) & value) != 0? 1 : 0) as number)
+                        .reduce((prevValue, currentValue) => prevValue + currentValue);
+                    text = `${count} item(s)`;
+                    break;
+                }
+            }
+            break;
+        }
     }
     return (
         <div className={props.className||""}>
