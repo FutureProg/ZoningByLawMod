f5c5737140b7c66f3d84de41d58de14373db2f65
Nick Morrison <nickmorrison09@gmail.com>
2024-08-10 21:01:58 -0400

    Created theme for round checkbox

--- UI/src/mods/components/ConstraintListItem/ConstraintListItem.tsx
+++ UI/src/mods/components/ConstraintListItem/ConstraintListItem.tsx
@@ -1,21 +1,27 @@
 import { ByLawItem, ByLawItemType } from 'mods/types';
 import styles from './ConstraintListItem.module.scss';
+import checkboxTheme from '../../themes/RoundCheckboxTheme.module.scss';
 import { VanillaComponentResolver } from 'vanillacomponentresolver';
 import { useState } from 'react';
 
-export const ConstraintListItem = (props: {itemType: ByLawItemType, value?: ByLawItem}) => {
-    
-    let [checked, setChecked] = useState(false);
+type ConstraintListItemProps = {
+    itemType: ByLawItemType,
+    value?: ByLawItem
+    onChecked?: (newValue: boolean) => void;
+}
 
-    let checkboxTheme = {
-        toggle: styles.checkboxToggle,
-        checkmark: styles.checkboxMark
-    }
-
+export const ConstraintListItem = (props: ConstraintListItemProps) => {
+    let enabled = props.value != undefined;
     return (
         <div className={styles.view}>
-            <VanillaComponentResolver.instance.Checkbox theme={checkboxTheme} onChange={() => setChecked(!checked)} checked={checked} />
-            {String(props.itemType)}
+            <div className={styles.infoRow}>
+                <VanillaComponentResolver.instance.Checkbox 
+                    theme={checkboxTheme} 
+                    onChange={() => props.onChecked && props.onChecked(!enabled)} 
+                    checked={enabled} 
+                />
+                <div className={styles.constraintName}>{String(props.itemType)}</div>
+            </div>            
         </div>
     )
 }
