c2c9eab6c6daf4e02ebbc895f3ddfb21d41fbe62
Nick Morrison <nickmorrison09@gmail.com>
2025-01-24 22:53:53 -0500

    Switched to just using JSX

--- UI/src/mods/components/Details/ByLawPropertyEditSection.tsx
+++ UI/src/mods/components/Details/ByLawPropertyEditSection.tsx
@@ -1,93 +1,108 @@
-import { ByLawConstraintType, ByLawItem, ByLawItemType } from "mods/types"
-import { Bounds1Field } from "../Bounds1Field";
+import { ByLawConstraintType, ByLawItem, ByLawItemType } from "mods/types";
 import { ByLawItemBounds1Editor } from "./ByLawItemBounds1Editor";
 import { Bounds1 } from "cs2/bindings";
-import { useState } from "react";
 import ByLawItemEnumEditor from "./ByLawItemEnumEditor";
 import { getMeasurementString } from "mods/utils";
-import { UnitSystem, useLocalization } from "cs2/l10n";
-import React from "react";
+import { useLocalization } from "cs2/l10n";
 
 type Props = {
-    byLawItem : ByLawItem;  
-    isOpen: boolean; 
+    byLawItem: ByLawItem;
+    isOpen: boolean;
     onChange?: (newItemValue: ByLawItem) => void;
 };
 
 /**
  * Responsible for choosing which editor to display based on the property type
  */
-export default ({byLawItem, isOpen, onChange: onChangeCallback}: Props) : JSX.Element => {  
-    let localization = useLocalization();  
-    const measurementSuffix = getMeasurementString(byLawItem.byLawItemType, byLawItem.constraintType);
+export default (
+    { byLawItem, isOpen, onChange: onChangeCallback }: Props,
+): JSX.Element => {
+    let localization = useLocalization();
+    const measurementSuffix = getMeasurementString(
+        byLawItem.byLawItemType,
+        byLawItem.constraintType,
+    );
     let isCellMeasurement = measurementSuffix.indexOf("cells") >= 0;
     if (!isOpen) {
-        return (<></>);
+        return <></>;
     }
 
-    let {constraintType: constraintType, byLawItemType: itemType} = byLawItem;
+    let { constraintType: constraintType, byLawItemType: itemType } = byLawItem;
 
-    if (constraintType == ByLawConstraintType.Length || constraintType == ByLawConstraintType.Count) {
-        
-        let onChange = (name: string, newValue: Bounds1) => {            
+    if (
+        constraintType == ByLawConstraintType.Length ||
+        constraintType == ByLawConstraintType.Count
+    ) {
+        let onChange = (name: string, newValue: Bounds1) => {
             // Handle conversions to metric
             if (isCellMeasurement) {
                 newValue = {
                     min: newValue.min * 8,
-                    max: newValue.max * 8                    
-                }
-            } else if (['m', 'ft'].includes(measurementSuffix) && localization.unitSettings.unitSystem == 1){
+                    max: newValue.max * 8,
+                };
+            } else if (
+                ["m", "ft"].includes(measurementSuffix) &&
+                localization.unitSettings.unitSystem == 1
+            ) {
                 newValue = {
                     min: Math.round(newValue.min / 3),
-                    max: Math.round(newValue.max / 3)                    
-                }
-            } 
+                    max: Math.round(newValue.max / 3),
+                };
+            }
             console.log(newValue);
             let nItemVal = {
                 ...byLawItem,
-                valueBounds1: newValue                
+                valueBounds1: newValue,
             };
             onChangeCallback && onChangeCallback(nItemVal);
-        }
+        };
         let boundsValue = byLawItem.valueBounds1;
         let step = 1;
         if (isCellMeasurement) {
             boundsValue = {
                 max: boundsValue.max / 8,
-                min: boundsValue.min / 8
-            }
-        } else if (['m', 'ft'].includes(measurementSuffix) && localization.unitSettings.unitSystem == 1){
+                min: boundsValue.min / 8,
+            };
+        } else if (
+            ["m", "ft"].includes(measurementSuffix) &&
+            localization.unitSettings.unitSystem == 1
+        ) {
             boundsValue = {
                 max: boundsValue.max * 3,
-                min: boundsValue.min * 3
-            }
+                min: boundsValue.min * 3,
+            };
             step = 3;
-        } 
-        return React.createElement(ByLawItemBounds1Editor, {
-            name: ByLawItemType[byLawItem.byLawItemType], 
-            bounds: boundsValue,   
-            step: step,
-            onChange
-        });
+        }
+        return (
+            <ByLawItemBounds1Editor
+                name={ByLawItemType[byLawItem.byLawItemType]}
+                bounds={boundsValue}
+                step={step}
+                onChange={onChange}
+            />
+        );
     }
 
-    if (constraintType == ByLawConstraintType.MultiSelect || constraintType == ByLawConstraintType.SingleSelect) {
+    if (
+        constraintType == ByLawConstraintType.MultiSelect ||
+        constraintType == ByLawConstraintType.SingleSelect
+    ) {
         let onChange = (nValue: number) => {
             let nItemVal = {
                 ...byLawItem,
-                valueByteFlag: nValue
-            }     
+                valueByteFlag: nValue,
+            };
             onChangeCallback && onChangeCallback(nItemVal);
-        }
+        };
 
         return (
             <ByLawItemEnumEditor
-            constraintType={byLawItem.constraintType}
-            itemType={byLawItem.byLawItemType}
-            itemValue={byLawItem.valueByteFlag}
-            onChange={onChange}
+                constraintType={byLawItem.constraintType}
+                itemType={byLawItem.byLawItemType}
+                itemValue={byLawItem.valueByteFlag}
+                onChange={onChange}
             />
-        )
+        );
     }
-    return (<></>);
-}
+    return <></>;
+};
