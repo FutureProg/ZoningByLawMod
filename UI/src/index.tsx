f743e9ee724177ecaef908f2865c8610e74cbba6
Nick Morrison <nickmorrison09@gmail.com>
2025-01-26 21:44:46 -0500

    Removed mod toggle from the zoning menu

--- UI/src/index.tsx
+++ UI/src/index.tsx
@@ -1,5 +1,4 @@
 import { ModRegistrar } from "cs2/modding";
-import { ConfigPanelToggleButton } from "mods/ConfigPanelToggleButton";
 import { MainModToggle } from "mods/MainModToggle";
 import { ModView } from "mods/ModView/ModView";
 import { VanillaComponentResolver } from "vanillacomponentresolver";
@@ -8,7 +7,6 @@ const register: ModRegistrar = (moduleRegistry) => {
     VanillaComponentResolver.setRegistry(moduleRegistry);
     //game-ui/game/components/asset-menu/asset-category-tab-bar/asset-category-tab-bar.tsx, AssetCategoryTabBar
     //game-ui/game/components/tool-options/mouse-tool-options/mouse-tool-options.tsx
-    moduleRegistry.extend("game-ui/game/components/tool-options/mouse-tool-options/mouse-tool-options.tsx", "MouseToolOptions", ConfigPanelToggleButton);
     moduleRegistry.append('Game', ModView);
     moduleRegistry.append("GameTopLeft", MainModToggle);
 }
