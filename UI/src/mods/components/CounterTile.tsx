52e345f8ea83c42e43ff47b6393a4ed5a50410ab
Nick Morrison <nickmorrison09@gmail.com>
2025-01-26 19:42:09 -0500

    Created a CounterTile component

---
+++ UI/src/mods/components/CounterTile.tsx
@@ -0,0 +1,30 @@
+import { Tooltip } from 'cs2/ui';
+import styles from './CounterTile.module.scss';
+import { CSSProperties } from 'react';
+import classNames from 'classnames';
+
+export const CounterTile = ({thresholds = {danger: 2, warning: 20}, ...props}: {
+    value: number,
+    iconSrc?: string,
+    hint?: string,
+    className?: string;
+    thresholds: { // if less than x, then change the colour
+        danger: number,
+        warning: number
+    }
+}) => {
+    const classes = classNames(styles.view,{
+        [props.className ?? 'null']: props.className != undefined,
+        [styles.danger]: props.value <= thresholds.danger,
+        [styles.warning]: props.value <= thresholds.warning && props.value > thresholds.danger
+    });
+    return (
+        <Tooltip tooltip={props.hint} disabled={!props.hint}>
+            <div className={classes}>
+                {props.iconSrc? <img src={props.iconSrc} /> : null }
+                <div>{props.value}</div>
+            </div>
+        </Tooltip>        
+    )
+
+}
