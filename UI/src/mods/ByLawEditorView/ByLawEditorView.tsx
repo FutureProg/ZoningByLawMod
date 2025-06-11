62b239035a9be41e184df13c10fb726a897343fe
Nick Morrison <nickmorrison09@gmail.com>
2025-06-11 18:13:12 -0400

    Revert "Showing Asset Packs in the Mod UI now"
    This reverts commit f143f6974eefbe657f43e57f49cbf859277255ff.

--- UI/src/mods/ByLawEditorView/ByLawEditorView.tsx
+++ UI/src/mods/ByLawEditorView/ByLawEditorView.tsx
@@ -89,6 +89,7 @@ export const ByLawEditorView = ({ searchQuery, selectedByLaw } : _Props) => {
         .map((key) => [key, key.split(/(?<![A-Z])(?=[A-Z])/).join(' ')] as [keyof typeof ByLawItemType, string])
         .filter(([key, readableName]) => searchQuery && readableName ? readableName.toUpperCase().indexOf(searchQuery.toUpperCase()) >= 0 : true);
     let listItems = types
+        .filter(([key, readableName]) => key != 'AssetPack') // TODO: Remove when AssetPacks are ready
         .map(([key, readableName]: [keyof typeof ByLawItemType, string], idx) =>
             <ConstraintListItem
                 key={idx}
