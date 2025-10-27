49d4af811c5f4fb8c69d12b7e163059849f70a39
Nick Morrison <nickmorrison09@gmail.com>
2025-10-27 19:57:56 -0400

    Add field data types for form handling and bind ByLawFields

--- UI/src/mods/bindings.ts
+++ UI/src/mods/bindings.ts
@@ -1,7 +1,61 @@
 import { bindMap, bindValue, trigger } from "cs2/api";
 import mod from '../../mod.json';
-import { ByLawZoneListItem, ZoningByLawBinding } from "./types";
+import { ByLawPropertyOperator, ByLawZoneListItem, ZoningByLawBinding } from "./types";
 import { Color, Entity } from "cs2/bindings";
+
+export type FieldDataBase = {
+    id: string;
+    label: string;
+    fieldType: "checkbox" | "radio" | "select" | "text" | "number" | "range";
+    options?: FieldDataOption[];
+    operatorOptions: ByLawPropertyOperator[];
+    selectedOperator: ByLawPropertyOperator;
+    value: any;
+}
+
+export type FieldDataOption = {
+    image?: string;
+    label: string;
+    value: any;
+}
+
+export type CheckboxFieldData = FieldDataBase & {
+    fieldType: "checkbox";
+    value: any[]; // array of selected values
+}
+
+export type RadioFieldData = FieldDataBase & {
+    fieldType: "radio";
+    value: any; // single selected value
+}
+
+export type SelectFieldData = FieldDataBase & {
+    fieldType: "select";
+    value: any; // single selected value
+}
+
+export type TextFieldData = FieldDataBase & {
+    fieldType: "text";
+    value: string;
+    validationRegex?: string;
+}
+
+export type NumberFieldData = FieldDataBase & {
+    fieldType: "number";
+    slider?: boolean;
+    value: number;
+    min?: number;
+    max?: number;
+    step?: number;
+}
+
+export type RangeFieldData = FieldDataBase & {
+    fieldType: "range";
+    value: [number, number]; // [min, max]
+    min?: number;
+    max?: number;
+    step?: number;
+}
 
 export const ZONE_COLOR_IDX = 0;
 export const ZONE_BORDER_IDX = 1;
@@ -15,6 +69,7 @@ export const selectedByLawColor$ = bindValue<Color[]>(mod.fullname, "SelectedByL
 export const selectedByLaw$ = bindValue<Entity>(mod.fullname, "SelectedByLaw");
 export const elligibleBuildingCount$ = bindValue<number>(mod.fullname, "ElligibleBuildings", -1);
 export const assetPackNameToHash$ = bindMap<string, number>(mod.fullname, "assetPackNameToHash");
+export const byLawFields$ = bindValue<any[]>(mod.fullname, "ByLawFields", []);
 
 export const setConfigPanelOpen = (open : boolean) => {
     trigger(mod.fullname, "SetConfigPanelOpen", open);
